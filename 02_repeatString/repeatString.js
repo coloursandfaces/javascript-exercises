7f7a0dd8c1443c5da6a02e8b03890a68dffadb48
coloursandfaces <victorouyang98@gmail.com>
2023-02-23 15:28:18 -0800

    Completed first and second exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
 
+    let hold = "";
+    
+    if (num < 0) {
+        return 'ERROR';
+    }
+
+    for (let i = 0; i < num; i++) {
+        hold += str;
+    }
+
+    return hold;
 };
 
 // Do not edit below this line
