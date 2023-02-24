44be5a767fb01e7f8c04309d4bc44e94ca4ca88e
coloursandfaces <victorouyang98@gmail.com>
2023-02-23 22:21:13 -0800

    Completed the Array removal exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
 
+    let remain = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (remove.includes(array[i])) {
+            remain;
+        } else {
+            remain.push(array[i]);
+        }
+    }
+    return remain;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
