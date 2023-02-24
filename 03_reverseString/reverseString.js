9b4a85dca9f211efec37334071cb99cb4878c9fe
coloursandfaces <victorouyang98@gmail.com>
2023-02-23 16:15:48 -0800

    Created a reverse string function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    let sliced = "";
+    let reverse = str.slice(-1);
+    let sliceBeg = -2
+    let sliceEnd = -1
+
+    for (let i = 0; i < str.length; i++) {
+        sliced = str.slice(sliceBeg, sliceEnd);
+        reverse += sliced;
+        sliceBeg--
+        sliceEnd--
+    }
+    return reverse;
 };
 
 // Do not edit below this line
