eb91e5eba92afa7416e97cc7d7504aaaf9f25f36
coloursandfaces <victorouyang98@gmail.com>
2023-02-27 15:10:16 -0800

    Completed tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let celsius = (farenheit - 32) * (5/9);
+  let celsiusRounded = Math.round(celsius * 10)/10;
+  return celsiusRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let farenheit = celsius * (9/5) + 32;
+  let farenheitRounded = Math.round(farenheit*10)/10;
+  return farenheitRounded;
 };
 
 // Do not edit below this line
