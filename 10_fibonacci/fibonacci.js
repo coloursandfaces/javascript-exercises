04658da4a155b6eedadf0e35fd0373a8c3bb8141
coloursandfaces <victorouyang98@gmail.com>
2023-03-09 16:57:40 -0800

    Completed fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let cur = 1
+    let prevSum = 0
 
+    if (num < 0) {
+        return "OOPS";
+    } else {
+        for (let i=2; i < num; i++) {
+            let next = cur + prevSum;
+            prevSum = cur;
+            cur = next;
+        } return prevSum + cur;
+    }
 };
 
 // Do not edit below this line
