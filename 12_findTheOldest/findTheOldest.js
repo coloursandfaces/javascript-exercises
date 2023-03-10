06923634b59eed6c1073df2ee6919e2d230d6965
coloursandfaces <victorouyang98@gmail.com>
2023-03-10 13:25:43 -0800

    completed the findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let today = new Date().getFullYear();
+    let oldestAge = 0;
+    let oldestPeople = [];
+    for (let person of arr) {
+        if (!('yearOfDeath' in person)) {
+            person.yearOfDeath = today;
+        }
+        let age = person.yearOfDeath - person.yearOfBirth;
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestPeople = [person];
+        } else if (age === oldestAge) {
+            oldestPeople.push(person);
+        }
+    }
+    return oldestPeople[0];
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
